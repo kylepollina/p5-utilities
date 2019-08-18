e32eeeffcb2b32e9776cf2f24911179cd88ac2d0
kylepollina <kylepollina@gmail.com>
2019-08-18 17:52:12 -0500

    Fixed bug in image.js - image -> img

--- image.js
+++ image.js
@@ -8,9 +8,9 @@ function getAvgColor(img, xmin, ymin, xmax, ymax) {
     let btot = 0;
     let total = 0;
 
-    for(let x = floor(xmin); 0 < x && x <= floor(xmax) && x <= img.width; x++) {
-        for(let y = floor(ymin); 0 < y && y <= floor(ymax) && y <= img.height; y++) {
-            let index = (x + y * image.width) * 4;
+    for(let x = floor(xmin); x < floor(xmax); x++) {
+        for(let y = floor(ymin); y < floor(ymax); y++) {
+            let index = (x + y * img.width) * 4;
 
             rtot += img.pixels[index];
             gtot += img.pixels[index+1];
